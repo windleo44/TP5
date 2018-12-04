2ae7604589ca818f08c84cc8927d844e3afb1ee4
windleo44 <leopold.bader@gmail.com>
2018-12-04 15:45:01 +0100

    Part1

--- arbitre.sh
+++ arbitre.sh
@@ -9,19 +9,25 @@ if [ -f listePID.txt ]; then
 	rm listePID.txt
 fi
 
-echo "8" >revolver.txt
-echo "PID arbitre : $$"
+echo 8 >revolver.txt
 
 X=$1
 posListe=1
+if [ posListe -ne 8 ]; then
+	PIDprochain= $(sed -n "$posListe$p" < ./listePID.txt)
+else;
+	posListe=1
+	PISprochain= $(sed -n "posListe$p" < ./listePID.txt)
+trap "kill PIDprochain" 31
 
 for ((i=0;i<X;i++)); do
     ./joueur.sh $$ &
+    kill -31 $!
     echo $! >listePID.txt
 done
 
 while true; do
-    sleep 1 &
+    sleep 6 &
     wait $!
 done
 
--- joueur.sh
+++ joueur.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
     echo "L'argument est invalide"<&2
     exit 1
 fi
-
+trap "kill -USR2 $1" USR2
 while true; do
     echo "I am stressed"
     echo "Le PID de mon arbitre est $1"
